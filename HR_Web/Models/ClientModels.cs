ba8787e8c6269d7ccae36bc2bf9a68859d4c5f0f
Harry Agung S <harry@invento.co.id>
2018-10-14 19:38:27 +0700

    1st init HR-Web

---
+++ HR_Web/Models/ClientModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HR_Web.Models
+{
+    [Table("Clients")]
+    public class ClientModels
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+
+        public string Address { get; set; }
+
+        [Display(Name = "Billing Address")]
+        public string BillingAddress { get; set; }
+
+        [Display(Name = "Contact")]
+        public string ContactPersonName { get; set; }
+
+        [Display(Name = "Phone 1")]
+        public string Phone1 { get; set; }
+
+        [Display(Name = "Phone 2")]
+        public string Phone2 { get; set; }
+        public string Notes { get; set; }
+        public string NPWP { get; set; }
+
+        [Display(Name = "NPWP Address")]
+        public string NPWPAddress { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ HR_Web/Models/HrContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HR_Web.Models
+{
+    public class HrContext : DbContext
+    {
+        public DbSet<ClientModels> Clients { get; set; }
+    }
+}
